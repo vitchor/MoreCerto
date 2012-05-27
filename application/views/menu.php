2558793d46206f3ac66ff884c09173c1cdb591ed
Gustavo <gustavozomer@gmail.com>
2012-05-27 02:03:51 -0300

    New layout files;

---
+++ application/views/menu.php
@@ -0,0 +1,26 @@
+<div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#"><img src="<?=base_url();?>img/logo.png"></img></a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="<?=base_url();?>">Home</a></li>
+              <li><a href="#about" data-toggle="modal" >Como Funciona</a></li>
+              <li><a href="#contact" data-toggle="modal">Contato</a></li>
+			  <li><a href="#contact" data-toggle="modal">Anuncie</a></li>
+			  <li class="like_button">
+			  	<fb:like href="http://www.morecerto.com.br" send="false" layout="button_count" width="60" show_faces="false" action="like" font="" class=" fb_edge_widget_with_comment fb_iframe_widget"></fb:like>
+			  </li>			  
+            </ul>
+            <ul class="nav pull-right">
+			  <li><a href="#contact" data-toggle="modal">Projeto Ganhador do Prêmio Sinapse de Inovação 2012</a></li>
+			</ul>
+          </div>
+        </div>
+      </div>
+    </div>
